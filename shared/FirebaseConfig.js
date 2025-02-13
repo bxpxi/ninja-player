691e99e870cc61a24406f43f6ee57e52d8a52b03
bxpxi <155725673+bxpxi@users.noreply.github.com>
2025-02-13 19:04:48 +0200

    Add files via upload

---
+++ shared/FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByHVCJWCzTXYWlaFt8ur7O_ZKugjiVW08",
+  authDomain: "ninja-player-bed50.firebaseapp.com",
+  projectId: "ninja-player-bed50",
+  storageBucket: "ninja-player-bed50.firebasestorage.app",
+  messagingSenderId: "113919524562",
+  appId: "1:113919524562:web:84bb94c92cd19710ec370b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
